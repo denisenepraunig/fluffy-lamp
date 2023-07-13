cdb4e45bcac6f5ecd4fd22de31ed998c1bf64d6e
Denise Nepraunig <denise.nepraunig@gmail.com>
2023-07-13 10:12:43 +0200

    initial files

---
+++ fluffy-lamp-example/fluffy-lamp-example/fluffy_lamp_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  fluffy_lamp_exampleApp.swift
+//  fluffy-lamp-example
+//
+//  Created by Denise Nepraunig on 13.07.23.
+//
+
+import SwiftUI
+
+@main
+struct fluffy_lamp_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
